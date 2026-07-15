ca41d2b4ee37a29b2843d61b02b646f167d59099
Pmorlando <Pmorlando67@gmail.com>
2026-07-14 23:45:34 -0600

    need to new templates

--- FinalProj/4cardmatch.cpp
+++ FinalProj/4cardmatch.cpp
@@ -11,8 +11,10 @@
 using namespace cv;
 using namespace std;
 
+
+/*
 vector<string> labels = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}; // all the card values
-string tempname = "temp.jpg";
+string tempname = "temp.JPG";
 // going to try a template of only 13 cards with the template only the corner of the card 
 struct Cardtemp {
     string value;
@@ -31,7 +33,9 @@ string Matchcard(Mat cardcorner)
     //add the matching stuff here 
     //return val; // returns string 
 }
-// better loading for templates 
+
+*/
+// better loading for all templates 
 /*
 vector<Cardtemp> loadtemp(vector<string> labels, string tempname) 
 {
@@ -53,17 +57,18 @@ vector<Cardtemp> loadtemp(vector<string> labels, string tempname)
 
 int main(int argc, char** argv)
 {
-    loadtemp(labels,tempname);
+    // addinhg in all templates for later testing
+    //loadtemp(labels,tempname);
 
     
 
 
-    Mat temp8 = imread("8temp.jpg", IMREAD_GRAYSCALE); // template file of cropped 8 corner
-    Mat temp9 = imread("9temp.jpg", IMREAD_GRAYSCALE); // template file of cropped 9 corner
-    Mat temp10 = imread("10temp.jpg", IMREAD_GRAYSCALE); // template file of cropped 10 corner
-    Mat tempQ = imread("Qtemp.jpg", IMREAD_GRAYSCALE); // template file of cropped Queen corner
+    Mat temp8 = imread("8temp.JPG", IMREAD_GRAYSCALE); // template file of cropped 8 corner
+    Mat temp9 = imread("9temp.JPG", IMREAD_GRAYSCALE); // template file of cropped 9 corner
+    Mat temp10 = imread("10temp.JPG", IMREAD_GRAYSCALE); // template file of cropped 10 corner
+    Mat tempQ = imread("Qtemp.JPG", IMREAD_GRAYSCALE); // template file of cropped Queen corner
     // need to add better way to load all the templates
-    Mat table = imread("table.jpg", IMREAD_GRAYSCALE); // table file for testing using upright images of cards
+    Mat table = imread("table4.jpg", IMREAD_GRAYSCALE); // table file for testing using upright images of cards
 
     if(temp8.empty() || temp9.empty() || temp10.empty() || tempQ.empty() || table.empty())
     {
@@ -72,11 +77,6 @@ int main(int argc, char** argv)
     }
     
     Mat result8, result9, result10, resultQ;
-    result8 = table.clone();
-    result9 = table.clone();
-    result10 = table.clone();
-    resultQ = table.clone();
-
     int method = TM_CCOEFF_NORMED;
 
     //will be replaced by the match function
@@ -94,19 +94,25 @@ int main(int argc, char** argv)
     minMaxLoc(result10, NULL, &maxval10, NULL, &maxloc10);
     minMaxLoc(resultQ, NULL, &maxvalQ, NULL, &maxlocQ);
 
-    printf("8 max %f, 9 max %f, 10 max %f, Queen max %f", maxval8, maxval9, maxval10, maxvalQ);
+    printf("8 max %f, 9 max %f, 10 max %f, Queen max %f\n", maxval8, maxval9, maxval10, maxvalQ);
+
+    Mat fin8, fin9, fin10, finQ;
+    fin8 = table.clone();
+    fin9 = table.clone();
+    fin10 = table.clone();
+    finQ = table.clone();
 
     // drawing just for testing now
-    rectangle(table, maxloc8, Point(maxloc8.x + temp8.cols, maxloc8.y + temp8.rows), Scalar(0, 255, 0), 2);
-    rectangle(table, maxloc9, Point(maxloc9.x + temp9.cols, maxloc9.y + temp9.rows), Scalar(0, 255, 0), 2);
-    rectangle(table, maxloc10, Point(maxloc10.x + temp10.cols, maxloc10.y + temp10.rows), Scalar(0, 255, 0), 2);
-    rectangle(table, maxlocQ, Point(maxlocQ.x + tempQ.cols, maxlocQ.y + tempQ.rows), Scalar(0, 255, 0), 2);
+    rectangle(fin8, maxloc8, Point(maxloc8.x + temp8.cols, maxloc8.y + temp8.rows), Scalar(0, 255, 0), 2);
+    rectangle(fin9, maxloc9, Point(maxloc9.x + temp9.cols, maxloc9.y + temp9.rows), Scalar(0, 255, 0), 2);
+    rectangle(fin10, maxloc10, Point(maxloc10.x + temp10.cols, maxloc10.y + temp10.rows), Scalar(0, 255, 0), 2);
+    rectangle(finQ, maxlocQ, Point(maxlocQ.x + tempQ.cols, maxlocQ.y + tempQ.rows), Scalar(0, 255, 0), 2);
 
     imshow("Blackjack Table", table);
-    imshow("8 Result", result8);
-    imshow("9 Result", result9);
-    imshow("10 Result", result10);
-    imshow("Q Result", resultQ);
+    imshow("8 Result", fin8);
+    imshow("9 Result", fin9);
+    imshow("10 Result", fin10);
+    imshow("Q Result", finQ);
 
 
     waitKey(0);
