b28290b5d79e3ce4be3a0a85831548daecd5f9a5
Pmorlando <73447662+Pmorlando@users.noreply.github.com>
2026-07-14 23:10:49 -0600

    updateRefactor card matching logic and improve comments

--- FinalProj/4cardmatch.cpp
+++ FinalProj/4cardmatch.cpp
@@ -1,4 +1,4 @@
-// written by by Phil Orlando for finalproj
+// written by by Phil Orlando for the Final Project
 //compiled with g++ -O0 -g -I/usr/local/include/opencv4 4cardmatch.cpp -o 4cardmatch -L/usr/local/lib `pkg-config --libs opencv4`
 
 
@@ -11,14 +11,58 @@
 using namespace cv;
 using namespace std;
 
+vector<string> labels = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}; // all the card values
+string tempname = "temp.jpg";
+// going to try a template of only 13 cards with the template only the corner of the card 
+struct Cardtemp {
+    string value;
+    Mat img;
+};
 
+// need to add
+// extract each card then use that in matchtemplate
+// read number of cardsb and then iterate through that 
+// assign location of the cards to dealer or player based on top or bottom of screen 
+// after extract card, correct it normal card orientation for the template match 
+
+
+string Matchcard(Mat cardcorner)
+{
+    //add the matching stuff here 
+    //return val; // returns string 
+}
+// better loading for templates 
+/*
+vector<Cardtemp> loadtemp(vector<string> labels, string tempname) 
+{
+    vector<Cardtemp> out;
+    for(auto& val : labels) 
+    {
+        Mat img = imread(val + tempname, IMREAD_GRAYSCALE);
+        if(img.empty()) 
+        { 
+            printf("failed to load %s\n", val.c_str()); 
+            continue;             
+        }
+        out.push_back({val, img});
+
+    }
+    return out;
+}
+*/
 
 int main(int argc, char** argv)
 {
+    loadtemp(labels,tempname);
+
+    
+
+
     Mat temp8 = imread("8temp.jpg", IMREAD_GRAYSCALE); // template file of cropped 8 corner
     Mat temp9 = imread("9temp.jpg", IMREAD_GRAYSCALE); // template file of cropped 9 corner
     Mat temp10 = imread("10temp.jpg", IMREAD_GRAYSCALE); // template file of cropped 10 corner
     Mat tempQ = imread("Qtemp.jpg", IMREAD_GRAYSCALE); // template file of cropped Queen corner
+    // need to add better way to load all the templates
     Mat table = imread("table.jpg", IMREAD_GRAYSCALE); // table file for testing using upright images of cards
 
     if(temp8.empty() || temp9.empty() || temp10.empty() || tempQ.empty() || table.empty())
@@ -35,6 +79,7 @@ int main(int argc, char** argv)
 
     int method = TM_CCOEFF_NORMED;
 
+    //will be replaced by the match function
     matchTemplate(table, temp8,  result8, method);
     matchTemplate(table, temp9,  result9, method);
     matchTemplate(table, temp10,  result10, method);
@@ -49,6 +94,9 @@ int main(int argc, char** argv)
     minMaxLoc(result10, NULL, &maxval10, NULL, &maxloc10);
     minMaxLoc(resultQ, NULL, &maxvalQ, NULL, &maxlocQ);
 
+    printf("8 max %f, 9 max %f, 10 max %f, Queen max %f", maxval8, maxval9, maxval10, maxvalQ);
+
+    // drawing just for testing now
     rectangle(table, maxloc8, Point(maxloc8.x + temp8.cols, maxloc8.y + temp8.rows), Scalar(0, 255, 0), 2);
     rectangle(table, maxloc9, Point(maxloc9.x + temp9.cols, maxloc9.y + temp9.rows), Scalar(0, 255, 0), 2);
     rectangle(table, maxloc10, Point(maxloc10.x + temp10.cols, maxloc10.y + temp10.rows), Scalar(0, 255, 0), 2);
@@ -61,7 +109,7 @@ int main(int argc, char** argv)
     imshow("Q Result", resultQ);
 
 
-    waitkey(0);
+    waitKey(0);
 
     return 0;
 }
