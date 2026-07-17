7bec41f6927d8d4701fcd02801a0a259bb66689e
Pmorlando <73447662+Pmorlando@users.noreply.github.com>
2026-07-17 16:19:49 -0600

    Fix filename case for temporary image

--- FinalProj/1cardmatch.cpp
+++ FinalProj/1cardmatch.cpp
@@ -16,7 +16,7 @@ using namespace std;
 string tablefile = "table7.jpg";
 
 vector<string> labels = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}; // all the card values
-string tempname = "temp.JPG";
+string tempname = "temp.jpg";
 // going to try a template of only 13 cards with the template only the corner of the card 
 struct Cardtemp {
     string value;
